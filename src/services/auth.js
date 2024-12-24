73e82c44df68d17b629d0cbad3591774dcd722a6
grazyfunk04 <kunalmehndi10@gmail.com>
2024-12-24 21:07:01 +0530

    commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth';
+const API_URL = process.env.REACT_APP_AUTH_URL;
 
 export const signup = async (userData) => {
     try {
